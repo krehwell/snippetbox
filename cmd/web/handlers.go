dd0ceff30c0762e5c9eab29be68d0a7e51caf78d
krehwell <fahrimuham001@gmail.com>
2023-03-10 20:30:48 +0700

    middleware for log and set secure header

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -15,6 +15,8 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	panic("opps somethings is wrong")
+
 	snippets, err := app.snippets.Latest()
 	if err != nil {
 		app.serverError(w, err)
